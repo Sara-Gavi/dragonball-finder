d03aacb95f990295ccf493b75d6cf8ad38811127
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-05-18 19:47:48 +0200

    Add logo that links to landing page and apply styles from my CSS guide

--- src/components/FinderPage.jsx
+++ src/components/FinderPage.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+import kintoApp from "../images/kinto_app.png";
+
 function FinderPage() {
-  return <p>Coming soon... Dragon Finder</p>;
+  return (
+    <div className="finder-page">
+      <p>Coming soon... Dragon Finder</p>
+
+      <div className="finder__logo-end">
+        <Link to="/">
+          <img src={kintoApp} alt="Volver al inicio" className="finder__logo" />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default FinderPage;
